2d70cf4edd3cf8e5d7ec7a410bb649a8f6f27ade
Daniel <danielchochlisnki@hotmail.com>
2023-08-25 01:20:37 +0200

    working react

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Controller/IndexController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        $numbers = [1, 2, 3, 5];
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+            'numbers' => $numbers, // Pass the 'numbers' variable to the template
+        ]);
+    }
+}
